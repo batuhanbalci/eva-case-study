10d53e1c00bc188e90c86204979e36767b2c0914
batuhan.balci <batuhanbalci@outlook.com>
2023-05-09 19:57:04 +0300

    config: init port changed

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3005;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
